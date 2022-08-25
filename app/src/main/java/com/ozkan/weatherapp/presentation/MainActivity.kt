a6f5cbf94732df07d43f5042d1232f8fa2923615
Ozkan Tuncel <ozkantuncel2016@gmail.com>
2022-08-25 16:07:03 +0300

    Created NavGraph and OnBoarding Screen

--- app/src/main/java/com/ozkan/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/ozkan/weatherapp/presentation/MainActivity.kt
@@ -7,28 +7,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.Modifier
-import  androidx.compose.foundation.layout.Column
-import androidx.compose.runtime.Composable
-import androidx.lifecycle.ViewModel
-import com.ozkan.weatherapp.presentation.ui.theme.DarkBlue
-import com.ozkan.weatherapp.presentation.ui.theme.DeepBlue
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.ozkan.weatherapp.navigation.SetupNavGraph
 import com.ozkan.weatherapp.presentation.ui.theme.WeatherAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private val viewModel: WeatherViewModel by viewModels()
     private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,43 +36,15 @@ class MainActivity : ComponentActivity() {
                 Manifest.permission.ACCESS_COARSE_LOCATION
             )
         )
+
         setContent {
             WeatherAppTheme {
-                MainPage(viewModel)
+
+                val navController = rememberNavController()
+                SetupNavGraph(navController = navController, startDestination = "welcome_screen",viewModel)
             }
         }
     }
 }
 
-@Composable
-fun MainPage(viewModel:WeatherViewModel){
-    Box(modifier = Modifier.fillMaxSize()) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(DarkBlue)
-        ) {
-            WeatherCard(
-                state = viewModel.state,
-                backgroundColor = DeepBlue
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            WeatherForecast(state = viewModel.state)
-        }
-    }
-    if (viewModel.state.isLoading) {
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            CircularProgressIndicator()
-        }
-    }
-    viewModel.state.error?.let { error ->
-        Text(
-            text = error,
-            color = Color.Red,
-            textAlign = TextAlign.Center
-        )
-    }
-}
+
